028def07cebbb21b14993e717ebf4ac9254394f8
Beatriz-AC <beatrizalmeidac001@gmail.com>
2025-12-09 11:12:58 -0300

    veneno

--- cardPokemon/assets/js/poison.js
+++ cardPokemon/assets/js/poison.js
@@ -103,18 +103,18 @@ card.classList.add("pokemon-card");
 
 
 const poisonPokemonList = [
-"bulbasaur",
-  "ivysaur",
+"Ekans",
+  "Arbok",
   "venusaur",
-  "weedle",
+  "nidoran",
   "tentacruel",
-  "stunky",
-  "gulpin",
-  "roselia",
-  "crobat",
-  "ariados",
-  "koffing",
-  "gengar",
+  "weezing",
+  "golbat",
+  "slowbro",
+  "Beedrill",
+  "Zubat",
+  "Nidoqueen",
+  "muk",
 ];
 
 
